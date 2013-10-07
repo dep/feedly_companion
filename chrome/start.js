0a9407f581949927c032fa8a492e869f27968c58
Danny Peck <dannypeck@gmail.com>
2013-10-07 15:01:16 -0400

    1.0.5 fixes an issue where the sharebox would sometimes not open.  shift-u shortcut for url only.

--- chrome/start.js
+++ chrome/start.js
@@ -1,23 +1,21 @@
-$("body").live("keyup", function(event) {
+$("body").live("keypress", function(event) {
     code = event.keyCode;
+    console.log(code);
 
-    if (nothing_focused()) {
-        if (code == 67 && event.shiftKey == true) {
-            var sharebox = $(".selectedEntry").find(".sharebox");
-            if (sharebox.length > 0) {
-                sharebox.remove();
-            } else {
+    if (nothing_focused() && event.shiftKey == true) {
+        if (code == 67) {
+            if (sharebox()) {
                 var title = generate_url();
-                $(".selectedEntry .title").after("<input type='text' class='sharebox' readonly>");
-                $(".selectedEntry .sharebox").attr("value", "'" + title.name + "' - From: " + title.url);
-                $(".selectedEntry .sharebox").select();
+                $(".selectedEntry .sharebox").attr("value", "'" + title.name + "' - From: " + title.url).select();
             }
-        } else if (code == "91" || code == "27") {
-            $(".selectedEntry .sharebox").slideUp(function() {
-                $(this).remove();
-            });
         }
-        if (code == 69 && event.shiftKey == true) {
+        if (code == 85) {
+            if (sharebox()) {
+                var title = generate_url();
+                $(".selectedEntry .sharebox").attr("value", title.url).select();
+            }
+        }
+        if (code == 69) {
             var title = generate_url();
             url = "https://mail.google.com/mail/?view=cm&fs=1&su=" + encodeURIComponent(title.name) + "&body=Check out this article: " + encodeURIComponent(title.name) + " From: " + title.url;
             chrome.extension.sendMessage({url: url}, function(response) { });
@@ -35,8 +33,17 @@ function generate_url() {
 
 // Check for focused elements
 function nothing_focused() {
-    var active_el = $(document.activeElement);
-    if (document.activeElement.tagName == "BODY") {
+    if (document.activeElement.tagName != "INPUT") {
+        return true;
+    }
+}
+
+function sharebox() {
+    var sharebox = $(".selectedEntry").find(".sharebox");
+    if (sharebox.length > 0) {
+        sharebox.remove();
+    } else {
+        $(".selectedEntry .title").after("<input type='text' class='sharebox' readonly>");
         return true;
     }
 }
